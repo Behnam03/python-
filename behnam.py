5fd45c47ffe54028cf9a34d92dbdf401668c2948
Behnam <93427904+Behnam03@users.noreply.github.com>
2021-11-02 17:13:14 +0100

    Add files via upload

---
+++ behnam.py
@@ -0,0 +1,26 @@
+j=int(input("Wie viele Gleichung hast du? "))
+i =1
+while i<=j :
+	print("Gleichung Nummer= ",i)
+	print(".                                        		                 ax^2+pX+q")
+	print("																					Quadratische Gleichungenrechnerisches Lösen")
+	a= float(input("Eingeben a= "))
+	p= float(input("Eingeben p= "))
+	q= float(input("Eingeben q= "))
+	print("																												X1=")
+	print("																												X2=")
+	p1=float(p/(2*a))
+	p2=float(p*p)
+	delta=float(p2-4*a*q)
+	if delta < 0 :
+		print("delta ",delta)
+		print("Keine Lösung")
+	else :
+		import math
+		delta1=float(math.sqrt(delta))
+		x1=float(-p1+(delta1/(2*a)))
+		x2=float(-p1-(delta1/(2*a)))
+		print("X1= ",x1)
+		print("X2= ",x2)
+	i=i+1
+print("Ende")
